f0ae169557412fe9b8af1533220d7596d940d247
James Hillyerd <james@hillyerd.com>
2021-01-18 19:46:43 -0800

    daemon: detect correct serial port

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,8 +1,32 @@
+use serialport::{SerialPortInfo, SerialPortType};
 use std::thread;
 use std::time::Duration;
 use systemstat::{data::CPULoad, Platform, System};
 
+const USB_VENDOR_ID: u16 = 0x1209; // pid.codes VID.
+const USB_PRODUCT_ID: u16 = 0x0001; // In house private testing only.
+
 fn main() {
+    if let Some(port) = detect_port() {
+        println!("port: {:?}", port);
+    }
+
+    print_load();
+}
+
+/// Looks for our monitor hardware on available serial ports.
+fn detect_port() -> Option<SerialPortInfo> {
+    // Detect serial port for monitor hardware.
+    let ports = serialport::available_ports().expect("No serial ports found!");
+
+    ports.into_iter().find(|p| match &p.port_type {
+        SerialPortType::UsbPort(info) => info.vid == USB_VENDOR_ID && info.pid == USB_PRODUCT_ID,
+        _ => false,
+    })
+}
+
+/// CPU load.
+fn print_load() {
     let sys = System::new();
     let cpu_load = sys.cpu_load().unwrap();
     let load_agg = sys.cpu_load_aggregate().unwrap();
