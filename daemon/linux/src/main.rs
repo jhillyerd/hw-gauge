e2115c216c53b614938a9849d1fe0a7756735a1f
James Hillyerd <james@hillyerd.com>
2022-10-04 20:51:26 -0700

    Add env_logger to linux daemon

--- daemon/linux/src/main.rs
+++ daemon/linux/src/main.rs
@@ -1,15 +1,15 @@
 use lib;
+use log::{info, warn};
 
 fn main() {
+    env_logger::init();
+
     loop {
         match lib::detectsend_loop() {
             Ok(()) => break,
             Err(e) => {
-                eprintln!(
-                    "Error: {:?}\nRetrying in {:?}...",
-                    e,
-                    lib::DETECT_RETRY_DELAY
-                );
+                warn!("Error: {:?}", e,);
+                info!("Retrying in {:?}", lib::DETECT_RETRY_DELAY);
             }
         }
         std::thread::sleep(lib::DETECT_RETRY_DELAY);
