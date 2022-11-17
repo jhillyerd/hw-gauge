4a0742090dab10fa30e1dbcf553496ed6445530a
James Hillyerd <james@hillyerd.com>
2022-11-16 17:20:51 -0800

    Add shell flake wrapper

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation rec {
-  name = "env";
-  env = buildEnv { name = name; paths = buildInputs; };
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
   buildInputs = [
     gdb
     libusb
